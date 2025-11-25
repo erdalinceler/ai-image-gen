e1154c858ea95e4f0e4a78529c064d4699a89ef7
erdalinceler <116901821+erdalinceler@users.noreply.github.com>
2025-11-25 16:03:45 +0300

    Add next.js top-loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Poppins } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
 
 const poppins = Poppins({
@@ -23,6 +24,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={poppins.variable} suppressHydrationWarning>
+          <NextTopLoader
+            color="#6366f1"
+            height={3}
+            showSpinner={false}
+          />
           {children}
         </body>
       </html>
